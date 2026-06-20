1fef6b053e97dac4f5a5aa874cc458df75b7a8b4
mostafa3sliaa <mostafa3sliaa@users.noreply.github.com>
2026-06-20 20:43:55 +0300

    fix: static export of sitemap and robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   // Using the domain defined in your layout.tsx metadataBase
   const baseUrl = 'https://mostafa-ahmed.dev';
